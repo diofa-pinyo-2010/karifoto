2fe0668c747b235d4a8c1a7c684e8dc104cf45d5
Richard Nemeth <nemethricsi@gmail.com>
2026-08-07 16:18:05 +0200

    add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
 
 import '@/app/globals.css';
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -22,7 +25,7 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
